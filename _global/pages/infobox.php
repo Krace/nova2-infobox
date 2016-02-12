1142c4d0b07f4dcd79b74ac6625dcdc08b2129e7
David VanScott <david.vanscott@gmail.com>
2016-02-12 08:59:16 -0500

    Clean up the infobox

--- _global/pages/infobox.php
+++ _global/pages/infobox.php
@@ -1,134 +1,80 @@
 <?php
-	/* Grab the information we want to display */
-	
-		if ($this->auth->is_logged_in() === TRUE)
-		{ /* if there's a session, set the variables appropriately */
-			/* pull all the info */
-				$session = $this->session->userdata('userid');
-				$userQuery = "SELECT name, date_of_birth, location, main_char FROM nova_users WHERE userid = '$session'";
-				$userfetch = mysql_query( $userQuery );
-				$userX = mysql_fetch_row( $userfetch );
-				$name = $userX[0];
-				$date_of_birth = $userX[1];
-				$location = $userX[2];
-				$main_char = $userX[3];
-		
-				$charQuery = "SELECT first_name, last_name, suffix, rank, position_1, position_2, images FROM nova_characters WHERE charid = '$main_char'";
-				$charfetch = mysql_query( $charQuery );
-				$charX = mysql_fetch_row( $charfetch );
-				$first_name = $charX[0];
-				$last_name = $charX[1];
-				$suffix = $charX[2];
-				$rank = $charX[3];
-				$position_1 = $charX[4];
-				$position_2 = $charX[5];
-				$imageX = $charX[6];
-				
-				$imgarr = explode(",", $imageX);
-				$char_image = $imgarr[0];
-		
-				$rankQuery = "SELECT rank_name FROM nova_ranks_ds9 WHERE rank_id = '$rank'";
-				$rankfetch = mysql_query( $rankQuery );
-				$rankX = mysql_fetch_row( $rankfetch );
-				$rank_name = $rankX[0];
-		
-				if ($position_1 > 0 === TRUE)
-				{
-					$posaQuery = "SELECT pos_name FROM nova_positions_ds9 WHERE pos_id = '$position_1'";
-					$posafetch = mysql_query( $posaQuery );
-					$posaX = mysql_fetch_row( $posafetch );
-					$posa_name = $posaX[0];
-				}
-				else
-				{
-					$posa_name = '';
-				}
-		
-				if ($position_2 > 0 === TRUE)
-				{
-					$posbQuery = "SELECT pos_name FROM nova_positions_ds9 WHERE pos_id = '$position_2'";
-					$posbfetch = mysql_query( $posbQuery );
-					$posbX = mysql_fetch_row( $posbfetch );
-					$posb_name = $posbX[0];
-				}
-				else
-				{
-					$posb_name = '';
-				}
-			
-		}
-		else
-		{ /* there's no session, so set variables as guest*/
-			$name = 'Guest';
-			$date_of_birth = '';
-			$location = '';
-			$char_image = 'GuestAv.png';
-			$rank_name = '';
-			$first_name = '';
-			$last_name = '';
-			$suffix = '';
-			$posa_name = '';
-			$posb_name = '';
-		}
-?>
 
-<?php
+if ($this->auth->is_logged_in())
+{
+	// Instead of manually querying, use the models to get the user
+	$user = $this->user->get_user($this->session->userdata('userid'));
 
-	/* Now display all the info we have gathered, but only if they have a value */
+	if ($user)
+	{
+		// Get the main character for the user
+		$character = $this->char->get_character($user->main_char);
+		
+		// Use the user object that's returned to fill the data
+		$name = $user->name;
+		$date_of_birth = $user->date_of_birth;
+		$location = $user->location;
+
+		// Get the character name with the rank, without the short rank, and with a link to the bio
+		$character_name = $this->char->get_character_name($character->charid, true, false, true);
+		
+		// You can grab the character images and explode at the same time
+		$imgarr = explode(",", $character->images);
+		$char_image = $imgarr[0];
+		
+		// Get the position names
+		$posa_name = $this->positions_model->get_position($character->position_1, 'pos_name');
+		$posb_name = $this->positions_model->get_position($character->position_2, 'pos_name');
+	}
 	
-		echo $name;
-		
-		if ($date_of_birth != '')
-		{
-			echo '<br />' . $date_of_birth;
-		}
-		if ($location != '')
-		{
-			echo '<br />' . $location;
-		}
-		if ($char_image != '')
-		{
-			echo 
-				'<div class="infobox-image-container">
-					<img src="' . base_url() . 'application/assets/images/characters/' . $char_image . '" class="infobox-image">
-				</div>';
-		} else { /* If the user has no image set, use the no-avatar image */
-			echo
-				'<div class="infobox-image-container">
-					<img src="' . base_url() . 'application/views/'.$current_skin.'/_global/images/no-avatar.png" class="infobox-image">
-				</div>';
-		}
-		
-		if ($rank_name != '')
-		{
-			$char_name = $rank_name;
-			if ($first_name != '' || $last_name != '')
-			{ $char_name .= ' ';}	
-		}
-		if ($first_name != '')
-		{
-			$char_name .= $first_name;
-			if ($last_name != '')
-			{ $char_name .= ' ';}
-		}
-		if ($last_name != '')
-		{
-			$char_name .= $last_name;
-		}
-		if ($suffix != '')
-		{
-			$char_name .= ' ' . $suffix;
-		}
-		if ($char_name != '')
-		{
-			echo anchor('personnel/character/'. $main_char, $char_name);
-		}
-		if ($posa_name != '')
-		{
-			echo '<br />' . $posa_name;
-		}
-		if ($posb_name != '')
-		{
-			echo '<br />' . $posb_name;
-		}
-?>
+	#TODO: you'll want to do something here in the (unlikely) scenario where there is no user object
+}
+else
+{
+	$name = 'Guest';
+	$date_of_birth = false;
+	$location = false;
+	$char_image = 'GuestAv.png';
+	$character_name = false;
+	$posa_name = false;
+	$posb_name = false;
+}
+
+echo $name;
+
+if ($date_of_birth)
+{
+	echo '<br />' . $date_of_birth;
+}
+
+if ($location)
+{
+	echo '<br />' . $location;
+}
+
+if ($char_image)
+{
+	echo 
+		'<div class="infobox-image-container">
+			<img src="' . base_url() . 'application/assets/images/characters/' . $char_image . '" class="infobox-image">
+		</div>';
+}
+else
+{
+	echo
+		'<div class="infobox-image-container">
+			<img src="' . base_url() . 'application/views/'.$current_skin.'/_global/images/no-avatar.png" class="infobox-image">
+		</div>';
+}
+
+echo $character_name;
+
+if ($posa_name)
+{
+	echo '<br />' . $posa_name;
+}
+
+if ($posb_name)
+{
+	echo '<br />' . $posb_name;
+}
